3c3736934b5a85ce28fd10d3ca6a8731b4e76c04
권재원 <springkjw@gmail.com>
2018-04-04 11:31:22 +0900

    s3 준비

--- assets/js/app.983d2bacdbba.js
+++
@@ -1,3 +0,0 @@
-(function(){$.ajaxSetup({beforeSend:function(e,t){/^http:.*/.test(t.url)||/^https:.*/.test(t.url)||e.setRequestHeader("X-CSRFToken",function(e){var t,n,o,u=null
-if(document.cookie&&""!=document.cookie)for(t=document.cookie.split(";"),n=0;n<t.length;n++)if((o=jQuery.trim(t[n])).substring(0,e.length+1)==e+"="){u=decodeURIComponent(o.substring(e.length+1))
-break}return u}("csrftoken"))}})}).call(this)
--- celuv/settings.py
+++ celuv/settings.py
@@ -119,61 +119,79 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'pipeline.finders.PipelineFinder',
-)
-PIPELINE = {
-    'COMPILERS': (
-        'pipeline.compilers.sass.SASSCompiler',
-        'pipeline.compilers.es6.ES6Compiler',
-    ),
-    'STYLESHEETS': {
-        'app': {
-            'source_filenames': (
-                'css/*.scss',
-            ),
-            'output_filename': 'css/app.css',
+if DEBUG:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+    STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+    STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        'pipeline.finders.PipelineFinder',
+    )
+    PIPELINE = {
+        'COMPILERS': (
+            'pipeline.compilers.sass.SASSCompiler',
+            'pipeline.compilers.es6.ES6Compiler',
+        ),
+        'STYLESHEETS': {
+            'app': {
+                'source_filenames': (
+                    'css/*.scss',
+                ),
+                'output_filename': 'css/app.css',
+            },
         },
-    },
-    'JAVASCRIPT': {
-        'app': {
-            'source_filenames': (
-                'js/*.js',
-            ),
-            'output_filename': 'js/app.js',
+        'JAVASCRIPT': {
+            'app': {
+                'source_filenames': (
+                    'js/*.js',
+                ),
+                'output_filename': 'js/app.js',
+            }
         }
     }
-}
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
-    'DEFAULT_PARSER_CLASSES': (
-        'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.MultiPartParser',
-        'rest_framework.parsers.FormParser',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-    'DATETIME_FORMAT': '%Y/%m/%d %H:%M'
-}
+    REST_FRAMEWORK = {
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+        ),
+        'DEFAULT_PARSER_CLASSES': (
+            'rest_framework.parsers.JSONParser',
+            'rest_framework.parsers.MultiPartParser',
+            'rest_framework.parsers.FormParser',
+        ),
+        'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.IsAuthenticated',
+        ),
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework.authentication.SessionAuthentication',
+            'rest_framework.authentication.BasicAuthentication',
+        ),
+        'DATETIME_FORMAT': '%Y/%m/%d %H:%M'
+    }
+else:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-AWS_ACCEE_KEY_ID = 'AKIAI7F2TBQGALFDSWEQ'
-AWS_SECRET_ACCESS_KEY = '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'
+    # AWS Setting
+    AWS_ACCEE_KEY_ID = 'AKIAI7F2TBQGALFDSWEQ'
+    AWS_SECRET_ACCESS_KEY = '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'
+    AWS_REGION = 'ap-northeast-2'
+    AWS_STORAGE_BUCKET_NAME = 'celuv'
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+    AWS_ACCESS_KEY_ID = 'ACCESS_KEY_ID'
+    AWS_SECRET_ACCESS_KEY = 'SECRET_ACCESS_KEY'
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    # Static Setting
+    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+    # Media Setting
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
