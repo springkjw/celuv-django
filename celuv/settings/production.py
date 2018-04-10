5732f68fe4496b819bfb5a65a5b00e918626c68c
권재원 <springkjw@gmail.com>
2018-04-11 05:58:51 +0900

    S3 설정

--- celuv/settings/production.py
+++ celuv/settings/production.py
@@ -13,25 +13,20 @@ DATABASES = {
         'HOST': 'celuv.cm4njigt3uj7.ap-northeast-2.rds.amazonaws.com'
     }
 }
-
-AWS_ACCESS_KEY_ID = 'AKIAIAMY5ESYHE5QC4NQ'
-AWS_SECRET_ACCESS_KEY = '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'
-
+AWS_REGION = 'ap-northeast-2'
 AWS_QUERYSTRING_AUTH = False
+AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
+AWS_S3_ACCESS_KEY_ID = 'AKIAIAMY5ESYHE5QC4NQ'
+AWS_S3_SECRET_ACCESS_KEY = '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'
 
-DEFAULT_FILE_STORAGE = 'celuv.utils.MediaRootS31otoStorage'
-STATICFILES_STORAGE = 'celuv.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'celuv'
-S3DIRECT_REGION = 'ap-northeast-2'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-MEDIA_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = MEDIA_URL
+# 정적 파일 설정
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-STATIC_URL = S3_URL + 'static/'
-STATICFILES_DIRS = [
-    os.path.join(settings.BASE_DIR, 'assets'),
-]
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-AWS_S3_HOST = 's3.%s.amazonaws.com' % S3DIRECT_REGION
+# 미디어 파일 설정
+MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
+MEDIA_ROOT = os.path.join(settings.BASE_DIR, 'media')
