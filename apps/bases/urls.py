867e13ba3c3888425aa41324b8eeb6062c8331db
권재원 <springkjw@gmail.com>
2018-04-12 02:29:18 +0900

    개인정보처리동의 페이지

--- apps/bases/urls.py
+++ apps/bases/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, PrivateView
 
 app_name = 'base'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('private', PrivateView.as_view(), name='private'),
 ]
--- apps/bases/views.py
+++ apps/bases/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 from django.urls import reverse_lazy
 
 
 class HomeView(RedirectView):
     url = reverse_lazy('celebrity:list')
+
+
+class PrivateView(TemplateView):
+    template_name = 'private.html'
