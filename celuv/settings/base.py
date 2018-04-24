ea9d0d25df946e8aba73531bbbb6d3c45bda3286
권재원 <springkjw@gmail.com>
2018-04-24 23:18:36 +0900

    디버그 레벨 수정

--- celuv/settings/base.py
+++ celuv/settings/base.py
@@ -170,7 +170,7 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
     'root': {
-        'level': 'WARNING',
+        'level': 'DEBUG',
         'handlers': ['sentry'],
     },
     'formatters': {
