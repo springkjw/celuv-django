6727ed9d96d099223ccdd755f295e440b294b8e8
권재원 <springkjw@gmail.com>
2018-04-24 22:37:19 +0900

    스케줄 필터링 버그 수정

--- api/schedules/views.py
+++ api/schedules/views.py
@@ -1,6 +1,8 @@
+from datetime import date, datetime, timedelta
 from rest_framework.generics import ListAPIView
 
 from apps.schedules.models import Schedule
+from apps.celebritys.models import Celebrity
 
 from .serializers import ScheduleSerializer
 
@@ -9,4 +11,40 @@ class ScheduleListAPIView(ListAPIView):
     # 스케줄 목록 API
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
-    
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user_like_celubs = self.request.user.celuv_user_likes.all()
+        queryset = queryset.filter(celeb__in=user_like_celubs)
+
+        celeb = self.request.GET.getlist('celeb', None)
+        date_type = self.request.GET.get('date_type', None)
+        req_date = self.request.GET.get('date', None)
+
+        if celeb:
+            selected_celubs = Celebrity.objects.filter(id__in=celeb)
+            queryset = queryset.filter(celeb__in=selected_celubs)
+
+        if not date_type and not req_date:
+            # filter by today
+            queryset = queryset.filter(
+                start__gte=date.today(),
+                start__lte=date.today() + timedelta(days=30)
+            ).order_by('schedule')
+        elif date_type:
+            if date_type == 'yesterday':
+                queryset = queryset.filter(
+                    start__date=date.today() - timedelta(days=1))
+            elif date_type == 'tomorrow':
+                queryset = queryset.filter(
+                    start__date=date.today() + timedelta(days=1))
+            elif date_type == 'week':
+                queryset = queryset.filter(
+                    start__week=date.today().isocalendar()[1]).order_by('schedule')
+        elif req_date:
+            queryset = queryset.filter(
+                start__date=datetime.strptime(req_date, "%Y-%m-%d").date())
+        else:
+            queryset = queryset.none()
+
+        return queryset
