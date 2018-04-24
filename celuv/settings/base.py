240bb64c5d560d9310139d245ce92ef1e6c044f4
권재원 <springkjw@gmail.com>
2018-04-24 23:31:15 +0900

    버그 수정

--- celuv/settings/base.py
+++ celuv/settings/base.py
@@ -30,6 +30,7 @@ PROJECT_APPS = [
     'apps.celebritys',
     'apps.schedules',
     'apps.entertainments',
+    'apps.feedbacks',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
