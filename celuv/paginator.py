61f229ceeaa79272edbcb0a62c1e88c70e148198
권재원 <springkjw@gmail.com>
2018-05-01 18:34:58 +0900

    페이지네이션 갯수 변경

--- celuv/paginator.py
+++ celuv/paginator.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 class StandardPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 100
     page_size_query_parm = 'page_size'
     max_page_size = 1000
 
