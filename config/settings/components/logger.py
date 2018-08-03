1fc999683826a3feb96daa4dc0d974368a9e4ad5
권재원 <springkjw@mintsage.co.kr>
2018-08-03 18:41:36 +0900

    도커 설정 추가

---
+++ config/settings/components/logger.py
@@ -0,0 +1,50 @@
+import raven
+
+if ENV != 'development':
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': True,
+        'root': {
+            'level': 'WARNING',
+            'handlers': ['sentry'],
+        },
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s '
+                        '%(process)d %(thread)d %(message)s'
+            },
+        },
+        'handlers': {
+            'sentry': {
+                'level': 'DEBUG',
+                'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+                'tags': {'custom-tag': 'x'},
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'django.db.backends': {
+                'level': 'ERROR',
+                'handlers': ['console'],
+                'propagate': False,
+            },
+            'raven': {
+                'level': 'DEBUG',
+                'handlers': ['console'],
+                'propagate': False,
+            },
+            'sentry.errors': {
+                'level': 'DEBUG',
+                'handlers': ['console'],
+                'propagate': False,
+            },
+        },
+    }
+
+RAVEN_CONFIG = {
+    'dsn': 'https://d6a09e8ec10c4af082e92a5224205c5a:e956906083884767bd1ff79d81dc61e8@sentry.io/1191144',
+}
